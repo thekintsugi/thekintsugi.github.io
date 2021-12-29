631f8e654e7560bc047f19f856850b713ae300b5
salmanhiro <salman.consulting@outlook.com>
2021-12-29 09:04:55 +0700

    add resources

--- js/main.js
+++ js/main.js
@@ -174,13 +174,13 @@
   });
 
   // Porfolio isotope and filter
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-item',
+  var portfolioIsotope = $('.resources-container').isotope({
+    itemSelector: '.resources-item',
     layoutMode: 'fitRows'
   });
 
-  $('#portfolio-flters li').on( 'click', function() {
-    $("#portfolio-flters li").removeClass('filter-active');
+  $('#resources-flters li').on( 'click', function() {
+    $("#resources-flters li").removeClass('filter-active');
     $(this).addClass('filter-active');
 
     portfolioIsotope.isotope({ filter: $(this).data('filter') });
